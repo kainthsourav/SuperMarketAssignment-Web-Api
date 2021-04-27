d4df6b05d814023625a37cad9e58774e915da95d
sourav <souravkainth@outlook.com>
2021-04-27 12:56:39 +0530

    Swagger

--- Supermarket.WebApi/Startup.cs
+++ Supermarket.WebApi/Startup.cs
@@ -26,6 +26,8 @@ namespace Supermarket.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +47,15 @@ namespace Supermarket.WebApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
